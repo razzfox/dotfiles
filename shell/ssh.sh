0e02f453e19e0846d80a531f1f311413491eeb13
Razz Fox <razzintown@gmail.com>
2015-02-22 21:23:31 -0700

    formatting

--- shell/ssh.sh
+++ shell/ssh.sh
@@ -5,22 +5,22 @@ chmod 0600 $HOME/.ssh/*
 
 # Optional SERVERS string array
 if test -f $HOME/.ssh/ssh_servers; then
-source $HOME/.ssh/ssh_servers
-for i in ${SERVERS[@]}; do
-  unset srv
-  unset nameup
-  unset namelow
+  source $HOME/.ssh/ssh_servers
+  for i in ${SERVERS[@]}; do
+    unset srv
+    unset nameup
+    unset namelow
 
-  srv="${i#*@}" # get substring after '@'
-  namelow="$( echo ${srv} | cut -d'.' -f1 | tr '[:upper:]' '[:lower:]' )"
-  nameup="$( echo ${namelow} | tr '[:lower:]' '[:upper:]' )"
+    srv="${i#*@}" # get substring after '@'
+    namelow="$( echo ${srv} | cut -d'.' -f1 | tr '[:upper:]' '[:lower:]' )"
+    nameup="$( echo ${namelow} | tr '[:lower:]' '[:upper:]' )"
 
-  if test "${srv##*.}" = "local"; then
-    declare ${nameup}LOCAL="${i}"
-    eval "ssh${namelow}local() { ssh ${i}; }"
-  else
-    declare ${nameup}="${i}"
-    eval "ssh${namelow}() { ssh ${i}; }"
-  fi
-done
+    if test "${srv##*.}" = "local"; then
+      declare ${nameup}LOCAL="${i}"
+      eval "ssh${namelow}local() { ssh ${i}; }"
+    else
+      declare ${nameup}="${i}"
+      eval "ssh${namelow}() { ssh ${i}; }"
+    fi
+  done
 fi
