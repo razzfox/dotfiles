58407ee97021aaeacf7e342e73fcc7931cd80c4e
Razz Fox <rfox@bigfootbiomedical.com>
2015-04-21 04:04:07 -0700

    made vl and shh non-root only

--- shell/ssh.sh
+++ shell/ssh.sh
@@ -1,26 +1,27 @@
+test $EUID = 0 && return 1
+
 # Protect ~/.ssh from other users and own group
 mkdir -p -m 0700 $HOME/.ssh
 chmod 0700 $HOME/.ssh
 chmod 0600 $HOME/.ssh/*
 
 # Optional SERVERS string array
-if test -f $HOME/.ssh/ssh_servers; then
-  source $HOME/.ssh/ssh_servers
-  for i in ${SERVERS[@]}; do
-    unset srv
-    unset nameup
-    unset namelow
+test -f $HOME/.ssh/ssh_servers && source $HOME/.ssh/ssh_servers || return
 
-    srv="${i#*@}" # get substring after '@'
-    namelow="$( echo ${srv} | cut -d'.' -f1 | tr '[:upper:]' '[:lower:]' )"
-    nameup="$( echo ${namelow} | tr '[:lower:]' '[:upper:]' )"
+for i in ${SERVERS[@]}; do
+  unset srv
+  unset nameup
+  unset namelow
 
-    if test "${srv##*.}" = "local"; then
-      declare ${nameup}LOCAL="${i}"
-      eval "ssh${namelow}local() { ssh ${i}; }"
-    else
-      declare ${nameup}="${i}"
-      eval "ssh${namelow}() { ssh ${i}; }"
-    fi
-  done
-fi
+  srv="${i#*@}" # get substring after '@'
+  namelow="$( echo ${srv} | cut -d'.' -f1 | tr '[:upper:]' '[:lower:]' )"
+  nameup="$( echo ${namelow} | tr '[:lower:]' '[:upper:]' )"
+
+  if test "${srv##*.}" = "local"; then
+    declare ${nameup}LOCAL="${i}"
+    eval "ssh${namelow}local() { ssh ${i}; }"
+  else
+    declare ${nameup}="${i}"
+    eval "ssh${namelow}() { ssh ${i}; }"
+  fi
+done
