d5733a56757ccf620bb9d1c03906d1319afe5a29
Razz Fox <razzintown@gmail.com>
2015-06-01 17:13:03 -0700

    Update convert_dotfiles_for_server.sh

--- howto/convert_dotfiles_for_server.sh
+++ howto/convert_dotfiles_for_server.sh
@@ -1,1 +1,1 @@
-cat dotfiles/shell/*.sh dotfiles/shell/*.linux dotfiles/shell/*.bash | grep -v "^which.*return.*" | grep -v "^pulse.*return.*" > servercopy.sh
+cat dotfiles/shell/*.sh dotfiles/shell/*.linux dotfiles/shell/*.bash | grep -v "^which.*return.*" | grep -v "^test.*return.*" | grep -v "^pulse.*return.*" > servercopy.sh
