36fc2f27d3a1f97fe135435a90339729c32dee01
Razz Fox <rfox@bigfootbiomedical.com>
2015-06-02 09:41:33 -0700

    Update convert_dotfiles_for_server.sh

--- howto/convert_dotfiles_for_server.sh
+++ howto/convert_dotfiles_for_server.sh
@@ -1,6 +1,10 @@
-cat dotfiles/shell/*.sh \
-  dotfiles/shell/*.bash \
-  dotfiles/shell/*.linux \
+head -n 15 "$DOTFILES"/shell/bashrc
+
+cat "$DOTFILES"/shell/*.sh \
+  "$DOTFILES"/shell/*.bash \
+  "$DOTFILES"/shell/*.linux \
   | grep -v "^which.*return.*" \
   | grep -v "^test.*return.*" \
   | grep -v "^pulse.*return.*"
+
+tail -n 5 "$DOTFILES"/shell/bashrc
