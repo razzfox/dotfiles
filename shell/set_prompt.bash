f50738cb1a9ba5a3d0dce5d3662364ee1047c831
Razz Fox <razzintown@gmail.com>
2015-06-02 13:04:35 -0700

    Update set_prompt.bash

--- shell/set_prompt.bash
+++ shell/set_prompt.bash
@@ -1,26 +1,27 @@
-# Relies on environment variables and functions sourced in distro-specific profile for 'git-prompt.sh'.
-
-if ! __git_ps1 >/dev/null; then
-  source /tmp/git-completion.bash /tmp/git-prompt.sh && return
-  
-  echo "Downloading 'git-completion.bash' and 'git-prompt.sh'" >/dev/stderr
-  curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >/tmp/git-completion.bash
-  curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh >/tmp/git-prompt.sh
-  source /tmp/git-completion.bash /tmp/git-prompt.sh
-fi
-
-color_word() {
-  HASH=$(echo $1 | tr -cd '[:alnum:].' | md5sum) # hash input
-  HASH=$(( 0x${HASH:1:3} % 7 + 1 )) # avoid negative sign and convert to decimal, mod a 3 digit number to get 1..7
-  echo -e "\033[$(( $HASH % 2 ));3${HASH}m$1\033[m" # print color codes
-}
-
 # GIT_PS1_SHOWSTASHSTATE=1
 # GIT_PS1_SHOWUNTRACKEDFILES=1
 # GIT_PS1_SHOWCOLORHINTS=1
 # GIT_PS1_DESCRIBE_STYLE="branch"
 # GIT_PS1_SHOWUPSTREAM="auto git"
 GIT_PS1_SHOWDIRTYSTATE=0
+
+color_word () {
+  HASH=$(echo $1 | tr -cd '[:alnum:].' | md5sum) # hash input
+  HASH=$(( 0x${HASH:1:3} % 7 + 1 )) # avoid negative sign and convert to decimal, mod a 3 digit number to get 1..7
+  echo -e "\033[$(( $HASH % 2 ));3${HASH}m$1\033[m" # print color codes
+}
+
+# Relies on otherwise sourced environment variables and functions sourced in distro-specific profile for 'git-prompt.sh'.
+download_git_functions () {
+  source /tmp/git-completion.bash /tmp/git-prompt.sh && continue
+  
+  echo "Downloading 'git-completion.bash' and 'git-prompt.sh'" >/dev/stderr
+  curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >/tmp/git-completion.bash
+  curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh >/tmp/git-prompt.sh
+  source /tmp/git-completion.bash /tmp/git-prompt.sh
+}
+
+__git_ps1 >/dev/null 2>/dev/null || download_git_functions
 
 set_prompt() {
   # NOTE: PS1 var requires '\[...\]' characters around color codes
