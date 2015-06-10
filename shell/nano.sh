5d16e0327a70e5c7ba2439e554c67bf6e968a1e8
Razz Fox <razzintown@gmail.com>
2015-06-09 17:20:11 -0700

    modified nano settings

--- shell/nano.sh
+++ shell/nano.sh
@@ -1,4 +1,8 @@
-mkdir -p $HOME/.nanorc_backups
+mkdir -p $HOME/.nanobackups
+if test ! -f $HOME/.nanorc; then
+  test -d /usr/local/share/nano && printf 'include %s\n' /usr/local/share/nano/* >> $HOME/.nanorc
+  test -d /usr/share/nano && printf 'include %s\n' /usr/share/nano/* >> $HOME/.nanorc
+fi
 
 nanosource() {
   nano "$@" && source "$@"
