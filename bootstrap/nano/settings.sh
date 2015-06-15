89dcbec157f11b0b03a626979cfd039e8b5633d6
Razz Fox <razzintown@gmail.com>
2015-06-15 12:24:41 -0700

    Update settings.sh

--- bootstrap/nano/settings.sh
+++ bootstrap/nano/settings.sh
@@ -1,7 +1,7 @@
 mkdir -p $HOME/.nanobackups
 chmod 700 $HOME/.nanobackups
 
-echo "set autoindent
+test -f $HOME/.nanorc || echo "set autoindent
 set morespace
 set nohelp
 set nowrap
@@ -13,5 +13,3 @@ set backupdir $HOME/.nanobackups
 test -f $HOME/.nanorc_bash && echo "include $HOME/.nanorc_bash" >> $HOME/.nanorc
 test -d /usr/local/share/nano && printf 'include %s\n' /usr/local/share/nano/* >> $HOME/.nanorc
 test -d /usr/share/nano && printf 'include %s\n' /usr/share/nano/* >> $HOME/.nanorc
-
-chmod 600 $HOME/.nanorc
