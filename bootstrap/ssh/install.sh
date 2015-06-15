ddad24aee97375d5526f17334ac59c6fad0878b7
Razz Fox <rfox@bigfootbiomedical.com>
2015-06-15 12:26:25 -0700

    Update install.sh

--- bootstrap/ssh/install.sh
+++ bootstrap/ssh/install.sh
@@ -3,9 +3,11 @@ mkdir -p -m 700 $HOME/.ssh
 chmod 700 $HOME/.ssh
 chmod 600 $HOME/.ssh/*
 
-test -f $HOME/.ssh/ssh_servers || echo 'SERVERS=(user@example.university.edu user@example.dhcp.io user@example.local)
+if test ! -f $HOME/.ssh/ssh_servers; then
+  echo 'SERVERS=(user@example.university.edu user@example.dhcp.io user@example.local)
 GH_WWW="https://github.com"
 GH_SSH="git@github.com"
 ' >> $HOME/.ssh/ssh_servers
+fi
 
 chmod 600 $HOME/.ssh/ssh_servers
