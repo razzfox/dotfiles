a63883e254e1910294f48bb1e25bc1fdbfb1a273
Razz Fox <rfox@bigfootbiomedical.com>
2015-06-15 12:05:19 -0700

    Update nano.sh

--- shell/nano.sh
+++ shell/nano.sh
@@ -1,8 +1,9 @@
 mkdir -p $HOME/.nanobackups
-chmod 0700 $HOME/.ssh
-chmod 0600 $HOME/.ssh/*
+chmod 0700 $HOME/.nanobackups
 
 if test ! -f $HOME/.nanorc; then
+  echo "set backup
+set backupdir $HOME/.nanobackups" >> $HOME/.nanorc
   test -d /usr/local/share/nano && printf 'include %s\n' /usr/local/share/nano/* >> $HOME/.nanorc
   test -d /usr/share/nano && printf 'include %s\n' /usr/share/nano/* >> $HOME/.nanorc
 fi
