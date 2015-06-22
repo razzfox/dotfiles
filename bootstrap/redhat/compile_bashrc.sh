34a0204370fa1fb17ef32bb7e99031e0115824dd
Razz Fox <rfox@bigfootbiomedical.com>
2015-06-22 15:12:13 -0700

    Update compile_bashrc.sh

--- bootstrap/redhat/compile_bashrc.sh
+++ bootstrap/redhat/compile_bashrc.sh
@@ -1,7 +1,5 @@
-# Copy your ''.USER_tmux.conf' separately and change these variables:
-# set -g default-command "${SHELL} --rcfile ~/.USER"
-# bind r source-file ~/.USER_tmux.conf
-
+# Copy your '.USER_tmux.conf' separately and add:
+# set -g default-command "$SHELL --rcfile ~/.${TMUX_SESSION}"
 
 # Output:
 echo "TMUX_CONF=.${USER}_tmux.conf
