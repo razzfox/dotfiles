fcf73e6b8948a96639ce40f92ef7105d77990c74
Razz Fox <razzintown@gmail.com>
2015-06-22 12:29:12 -0700

    Update and rename howto/rpi.txt to bootstrap/rpi/install.sh

--- bootstrap/rpi/install.sh
+++ bootstrap/rpi/install.sh
@@ -1,11 +1,11 @@
-## Sound
+# Sound Install
 pacman -S alsa-firmware alsa-lib alsa-plugins alsa-utils
+
 echo "snd-bcm2835" >> /etc/modules-load.d/snd-bcm2835.conf 
 modprobe snd-bcm2835
+
 alsamixer
 speaker-test -c 2
-# save adjustments
-# alsactl store
 
-## Install
-pacman -S dosfstools
+# save sound adjustments
+#alsactl store
