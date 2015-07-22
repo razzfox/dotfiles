f6b03a922124b2f9debba81c60d418fb8fa99bcb
Razz Fox <razzintown@gmail.com>
2015-07-22 13:53:35 -0700

    some hlwm improvements and changes to how git-prompt is sourced

--- shell/command_not_found_handle.bash
+++ shell/command_not_found_handle.bash
@@ -1,4 +1,4 @@
 command_not_found_handle() {
-  echo "That isn't a real command. Jesus Christ, $1."
+  echo "bash: command_not_found_handle: That isn't a real command: $1" >/dev/stderr
   return 127
 }
