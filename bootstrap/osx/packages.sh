4d9ff2190b58c9e1c4f45ea7a01869a48201a935
Razz Fox <razzintown@gmail.com>
2015-07-23 11:23:50 -0700

    use brew osx to install quicklook plugins

--- bootstrap/osx/packages.sh
+++ bootstrap/osx/packages.sh
@@ -7,8 +7,9 @@ brew tap laurent22/massren && brew install massren
 brew tap jlhonora/lsusb && brew install lsusb
 
 # Use 'cask' to download typical mac apps
-#brew install caskroom/cask/brew-cask
-#brew cask install google-chrome android-studio atom iterm2 github audacity clipmenu
+brew install caskroom/cask/brew-cask
+#brew cask install google-chrome android-studio atom iterm2 audacity clipmenu
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
 # Always run as root
 sudo chown root:wheel /usr/local/Cellar/htop-osx/*/bin/htop
