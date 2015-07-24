5041c29f5fc46c36e8b0ecc6df8c9f74cc5358c8
Razz Fox <rfox@bigfootbiomedical.com>
2015-07-24 16:09:25 -0700

    Update settings.sh

--- bootstrap/nano/settings.sh
+++ bootstrap/nano/settings.sh
@@ -2,8 +2,8 @@ mkdir -p $HOME/.config/nano/backups
 chmod 700 $HOME/.config/nano/backups
 
 if test ! -e $HOME/.config/nano/bash.nanorc; then
-  echo 'syntax "bash" "\.(bash|zsh|fish|arch|linux|osx|darwin|cygwin|windows)$"' >$HOME/.config/nano/bash.nanorc
-  cat /usr/local/share/nano/sh.nanorc /usr/share/nano/sh.nanorc 2>/dev/null | tail -n +4 >>$HOME/.config/nano/bash.nanorc
+  cat $(echo 'syntax "bash" "\.(bash|zsh|fish|arch|linux|osx|darwin|cygwin|windows)$"') \
+  /usr/local/share/nano/sh.nanorc /usr/share/nano/sh.nanorc 2>/dev/null | tail -n +4 >$HOME/.config/nano/bash.nanorc
 fi
 
 if test ! -e $HOME/.nanorc; then
