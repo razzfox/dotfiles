59ccafc07fa17c614ebe1e6202919ce2b0502131
Razz Fox <razzintown@gmail.com>
2015-07-28 15:14:00 -0700

    Update compile_bashrc.sh

--- bootstrap/redhat/compile_bashrc.sh
+++ bootstrap/redhat/compile_bashrc.sh
@@ -13,4 +13,4 @@ cat "$DOTFILES"/shell/profile "$DOTFILES"/shell/{*.bash,*.sh,*.redhat,*.linux} \
 
 # Tmux_conf output
 echo "set -g default-command \"$SHELL --rcfile ~/.${TMUX_SESSION}\"" >$HOME/.${USER}_tmux.conf
-cat "$DOTFILES"/config/arch/home/razz/.tmux_conf >>$HOME/.${USER}_tmux.conf
+cat "$DOTFILES"/config/arch/home/razz/.tmux.conf >>$HOME/.${USER}_tmux.conf
