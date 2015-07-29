68fa3b324a50d8eae67f94912401576f65a735a3
Razz Fox <razzintown@gmail.com>
2015-07-29 12:25:28 -0700

    Create remove_bloatware.sh

---
+++ bootstrap/osx/remove_bloatware.sh
@@ -0,0 +1,5 @@
+# Remove bloatware
+sudo rm -rf /Applications/Calendar.app /Applications/Chess.app /Applications/Contacts.app \
+/Applications/Dashboard.app /Applications/FaceTime.app /Applications/Game\ Center.app \
+/Applications/Mail.app /Applications/Mission\ Control.app /Applications/Notes.app \
+/Applications/Stickies.app /Applications/Reminders.app /Applications/iTunes.app /Applications/Messages.app
