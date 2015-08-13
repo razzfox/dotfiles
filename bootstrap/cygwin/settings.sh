866bf8580619a54244ff2769c7d89a3bbade3d37
Razz Fox <razzintown@gmail.com>
2015-08-13 15:28:24 -0700

    Update settings.sh

--- bootstrap/cygwin/settings.sh
+++ bootstrap/cygwin/settings.sh
@@ -1,2 +1,7 @@
-# Set Home directory to windows user home
+# Set cygwin home directory to Windows user home
 exec mkpasswd -l -c -p "$(cygpath -H)" > /etc/passwd
+
+# Links
+ln -s AppData/Local/Microsoft/Windows/Themes
+ln -s AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup
+ln -s AppData/Roaming/Microsoft/Internet\ Explorer/Quick\ Launch/User\ Pinned/TaskBar
