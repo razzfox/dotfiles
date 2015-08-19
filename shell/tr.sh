2dc29a1310139584ad05146015c8e0810bdc355c
Razz Fox <rfox@bigfootbiomedical.com>
2015-08-19 14:21:35 -0700

    Create tr.sh

---
+++ shell/tr.sh
@@ -0,0 +1,7 @@
+tr_dv_qwerty () {
+  tr "',.pyfgcrl/=oeuidhtns\-;qjkxbmwvz" "qwertyuiop[]sdfghjkl;'zxcvbnm,./" "$@"
+}
+
+tr_qwerty_dv () {
+  tr "qwertyuiop[]sdfghjkl;'zxcvbnm,./" "',.pyfgcrl/=oeuidhtns\-;qjkxbmwvz" "$@"
+}
