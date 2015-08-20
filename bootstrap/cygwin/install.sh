fa5ccfb8a823076ccc2d808e1ce106fafef3561a
Razz Fox <razzintown@gmail.com>
2015-08-20 10:11:06 -0700

    Update install.sh

--- bootstrap/cygwin/install.sh
+++ bootstrap/cygwin/install.sh
@@ -2,3 +2,22 @@ cd /opt
 
 git clone https://github.com/transcode-open/apt-cyg.git
 cp -v apt-cyg/apt-cyg /usr/local/bin/
+
+# git clone https://github.com/cacalabs/libcaca.git
+# Libcaca from here instead:
+#http://caca.zoy.org/wiki/libcaca
+# ./bootstrap && ./configure && make && make install
+
+
+
+# Also install:
+# Go
+#https://golang.org/dl/
+# Ruby
+#http://rubyinstaller.org/
+# TeraCopy
+#http://www.codesector.com/downloads
+# ffmpeg
+#http://ffmpeg.zeranoe.com/builds/
+# Ext2Fsd
+#http://www.ext2fsd.com/
