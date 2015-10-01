39e38638d2eeb5aed28704ef2c7505dc57d52687
Razz Fox <rfox@bigfootbiomedical.com>
2015-10-01 14:18:06 -0700

    working

--- shell/git.sh
+++ shell/git.sh
@@ -31,6 +31,45 @@ git_search_source_history () {
   git grep "$1" $(git rev-list --all)
 }
 
+__gg_commit_message () {
+# Please write good git commit messages.  A good commit message
+# looks like this:
+#
+# 	Header line: explain the commit in one line (use the imperative)
+#
+# 	Body of commit message is a few lines of text, explaining things
+# 	in more detail, possibly giving some background about the issue
+# 	being fixed, etc etc.
+#
+# 	The body of the commit message can be several paragraphs, and
+# 	please do proper word-wrap and keep columns shorter than about
+# 	74 characters or so. That way "git log" will show things
+# 	nicely even when it's indented.
+#
+# 	Make sure you explain your solution and why you're doing what you're
+# 	doing, as opposed to describing what you're doing. Reviewers and your
+# 	future self can read the patch, but might not understand why a
+# 	particular solution was implemented.
+#
+# 	Reported-by: whoever-reported-it
+# 	Signed-off-by: Your Name <youremail@yourhost.com>
+#
+# where that header line really should be meaningful, and really should be
+# just one line.  That header line is what is shown by tools like gitk and
+# shortlog, and should summarize the change in one readable line of text,
+# independently of the longer explanation. Please use verbs in the
+# imperative in the commit message, as in "Fix bug that...", "Add
+# file/feature ...", or "Make Subsurface..."
+# Linus Torvalds, Sep 6, 2011: https://github.com/torvalds/subsurface/blob/master/README#L88
+# Add some information about properly formatted commit messages
+# It does seem like a lot of github users are not used to good commit
+# message rules, and may never have used git for a project that actually
+# cares about good logs and nice summary lines.
+#
+# Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
+true
+}
+
 gg() {
   # Test if gitconfig is set up
   grep -F name $HOME/.gitconfig && grep -F email $HOME/.gitconfig
