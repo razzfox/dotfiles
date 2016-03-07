23710b9a62489f48b8582969f99a0ee07ac5077d
Razz Fox <razzintown@gmail.com>
2016-03-07 03:20:00 -0700

    fix dotfiles-sync

--- bootstrap/ssh/settings.sh
+++ bootstrap/ssh/settings.sh
@@ -3,6 +3,7 @@ mkdir -p -m 700 $HOME/.ssh
 chmod 700 $HOME/.ssh
 chmod 600 $HOME/.ssh/*
 
+# Used by shell/dotfiles-sync.sh and shell/ssh.sh
 if test ! -f $HOME/.ssh/ssh_servers; then
   echo 'SSH_SERVERS=(user@example.university.edu user@example.dhcp.io user@example.local)
 GH="https://github.com""
