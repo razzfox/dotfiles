74d839151386f5b6cd708edd6d4001449a35c1da
Razz Fox <rfox@bigfootbiomedical.com>
2016-03-18 07:06:50 -0700

    workaround for debian

--- shell/set_term.sh
+++ shell/set_term.sh
@@ -1,12 +1,5 @@
 TERMINFO="$HOME/.terminfo"
 
-# Remove smcup and rmcup capabilites (switches to and from alternate screen)
-if test ! -f "$TERMINFO/${TERM::1}/$TERM"; then
-  infocmp | sed -e 's/[sr]mcup=[^,]*,//' >$HOME/.terminfo-noaltscreen-$TERM
-  tic $HOME/.terminfo-noaltscreen-$TERM
-#  rm $HOME/.terminfo-noaltscreen-$TERM
-fi
-
 case "$TERM" in
   linux)
     # Num lock on
@@ -21,7 +14,7 @@ case "$TERM" in
     echo -n $(tput smkx) >/dev/tty # enable delete key on st-terminal
     setterm -blank 0 -powerdown 0 -powersave off
     ;;
- tmux-256color)
+  tmux-256color)
     echo -n $(tput smkx) >/dev/tty # enable delete key on st-terminal
     setterm -blank 0 -powerdown 0 -powersave off
     tmux set -g window-status-current-format "#[fg=colour234,bg=colour$(color_number $HOSTNAME),noreverse,bold] #I #W "
@@ -29,5 +22,18 @@ case "$TERM" in
     ;;
   *)
     setterm -blank 0 -powerdown 0 -powersave off
+    ;;
+esac
+
+case "$TERM" in
+  tmux-256color)
+    ;;
+  *)
+# Remove smcup and rmcup capabilites (switches to and from alternate screen)
+if test ! -f "$TERMINFO/${TERM::1}/$TERM"; then
+  infocmp | sed -e 's/[sr]mcup=[^,]*,//' >$HOME/.terminfo-noaltscreen-$TERM
+  tic $HOME/.terminfo-noaltscreen-$TERM
+#  rm $HOME/.terminfo-noaltscreen-$TERM
+fi
     ;;
 esac
