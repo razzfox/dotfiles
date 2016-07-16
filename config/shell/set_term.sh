ab3e80e0517a9a54e5a96f3121984e33af023c51
Razz Fox <razzintown@gmail.com>
2016-07-16 11:58:42 -0700

    fix temp files to be in home folder and turn on 256 colors

--- config/shell/set_term.sh
+++ config/shell/set_term.sh
@@ -7,31 +7,21 @@ case "$TERM" in
 
     # Terminal sleep settings
     # do not blank the screen on linux framebuffer
-    setterm -powersave off
+    setterm -blank 0 -powerdown 0 -powersave off >/dev/null
     ;;
-  st-256color)
-    export TERM=xterm-256color
+  st-256color|tmux-256color|screen-256color)
     echo -n $(tput smkx) >/dev/tty # enable delete key on st-terminal
-    setterm -blank 0 -powerdown 0 -powersave off
-    ;;
-  tmux-256color)
-    echo -n $(tput smkx) >/dev/tty # enable delete key on st-terminal
-    setterm -blank 0 -powerdown 0 -powersave off
+    setterm -blank 0 -powerdown 0 -powersave off >/dev/null
     ;;
   *)
-    setterm -blank 0 -powerdown 0 -powersave off
+    echo -n $(tput smkx) >/dev/tty # enable delete key on st-terminal
+    setterm -blank 0 -powerdown 0 -powersave off >/dev/null
     ;;
 esac
 
-case "$TERM" in
-  tmux-256color)
-    ;;
-  *)
 # Remove smcup and rmcup capabilites (switches to and from alternate screen)
 if test ! -f "$TERMINFO/${TERM::1}/$TERM"; then
-  infocmp | sed -e 's/[sr]mcup=[^,]*,//' >$HOME/.terminfo-noaltscreen-$TERM
+  infocmp | sed -e 's/[sr]mcup=[^,]*,//' > $HOME/.terminfo-noaltscreen-$TERM
   tic $HOME/.terminfo-noaltscreen-$TERM
-#  rm $HOME/.terminfo-noaltscreen-$TERM
+  rm $HOME/.terminfo-noaltscreen-$TERM
 fi
-    ;;
-esac
