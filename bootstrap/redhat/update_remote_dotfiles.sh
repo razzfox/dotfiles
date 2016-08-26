ffaa360ce72b6f319cb7e756f641af5b097974b8
Razz Fox <rfox@bigfootbiomedical.com>
2016-08-25 18:32:29 -0700

    replace '~/' with '$HOME' for portability

--- bootstrap/redhat/update_remote_dotfiles.sh
+++ bootstrap/redhat/update_remote_dotfiles.sh
@@ -3,6 +3,6 @@ git checkout -f
 git pull
 cd
 source dotfiles/bootstrap/redhat/compile_bashrc.sh
-rsync .tmux.conf $1:~/.${USER}_tmux.conf
-rsync .$USER $1:~/
+rsync .tmux.conf $1:$HOME/.${USER}_tmux.conf
+rsync .$USER $1:$HOME/
 rm .$USER
--- config/tmux/style_hostname.sh
+++ config/tmux/style_hostname.sh
@@ -2,16 +2,16 @@
 
 # Impossible due to bash bug where it adds quotes around the subshell output
 # Also tmux will not accept fd as source-file
-#tmux $( grep " pane-active-border-fg " ~/.tmux.conf | sed "s/colour[0-9]*/colour$(color_number $HOSTNAME)/" )
-#tmux $( grep " window-status-current-format " ~/.tmux.conf | sed "s/bg=colour[0-9]*/bg=colour$(color_number $HOSTNAME)/" )
-#tmux $( grep " status-right " ~/.tmux.conf | sed "s/bg=colour[0-9]*/bg=colour$(color_number $HOSTNAME)/" )
+#tmux $( grep " pane-active-border-fg " $HOME/.tmux.conf | sed "s/colour[0-9]*/colour$(color_number $HOSTNAME)/" )
+#tmux $( grep " window-status-current-format " $HOME/.tmux.conf | sed "s/bg=colour[0-9]*/bg=colour$(color_number $HOSTNAME)/" )
+#tmux $( grep " status-right " $HOME/.tmux.conf | sed "s/bg=colour[0-9]*/bg=colour$(color_number $HOSTNAME)/" )
 
 # Turns out this wasn't necessary due to 'tmux show-options -g'
 #grep " pane-active-border-fg " "$TMUX_CONF" | sed "s/colour[0-9]*/colour$(color_number $HOSTNAME)/" >> $HOME/style_hostname
 #grep " window-status-current-format " "$TMUX_CONF" | sed "s/bg=colour[0-9]*/bg=colour$(color_number $HOSTNAME)/" >> $HOME/style_hostname
 #grep " status-right " "$TMUX_CONF" | sed "s/bg=colour[0-9]*/bg=colour$(color_number $HOSTNAME)/" | tr \" \' >> $HOME/style_hostname
 
-source ~/.config/shell/set_prompt.bash
+source $HOME/.config/shell/set_prompt.bash
 
 # It is necessary to do this way because I only want to change the color, not the existing format
 echo -n "set -g " >> $HOME/style_hostname
