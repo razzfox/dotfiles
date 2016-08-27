2b2e2199a35603e657fe9b6736d2835020958d79
Razz Fox <razzintown@gmail.com>
2016-08-27 14:07:59 -0700

    clarified nanodotfiles from nanosource

--- config/shell/nano.sh
+++ config/shell/nano.sh
@@ -1,11 +1,31 @@
 source "${DOTFILES:-$HOME/dotfiles}"/bootstrap/nano/settings.sh
 
 nanosource() {
-  nano "${DOTFILES}/config/shell/$@" && for i in "${DOTFILES}/config/shell/$@" ; do source "$i" ; done
+  nano "$1" && source "$1"
 }
 
-nanocat() {
-  nano "$@" && cat "$@"
+nanodotfiles() {
+  for i in "$@" ; do
+    file="${DOTFILES}/config/shell/${i}"
+    if test -f ${file}* 2>/dev/null ; then
+      nanosource ${file}*
+    else
+      echo "Press Ctrl-C to exit."
+      select j in ${file}*; do
+        if echo ${file}* | grep "$j" ; then
+          nanosource "$j"
+        fi
+        break
+#        case "$j" in
+#          "")
+#            break
+#            ;;
+#          *)
+#            ;;
+#        esac
+      done
+    fi
+  done
 }
 
 nanoexec() {
