d2b62cd907ea731054b7caa54ceb80c0e37f5d3f
Razz Fox <razzintown@gmail.com>
2016-08-27 16:27:11 -0700

    merge rmdir to rm and make nanodotfiles work with multiple files

--- config/shell/rm.sh
+++ config/shell/rm.sh
@@ -16,6 +16,23 @@ rm () {
   $( which rm ) -vI "$@"
 }
 
+rmdir () {
+  if test $# -gt 3 ; then
+    for i in "$@" ; do
+      echo -n "'$i' "
+    done
+
+    echo
+  fi
+
+  echo $( which rmdir ) -v "$@"
+  $( which rmdir ) -v "$@"
+  
+  if test $? -ne 0 ; then
+    ls -lah "$@" | less
+  fi
+}
+
 trash () {
   TRASH=$HOME/.trash/$( date +%F-%H-%M-%S )
   mkdir -p $TRASH
