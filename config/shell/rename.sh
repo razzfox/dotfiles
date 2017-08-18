11a02af149662064314bbd42ab45047b235dc3b5
Razz Fox <rfox@bigfootbiomedical.com>
2017-08-18 04:45:51 -0700

    rename script is smarter

--- config/shell/rename.sh
+++ config/shell/rename.sh
@@ -1,9 +1,26 @@
-rename() {
-  test -e "$2" -o ! -e "$1" || return 2
-  test -d "$2" -a ! -d "$1" && return 3
-  mv -vi "$1" "$2"
+rename () {
+  # 2 should never exist anyway, but its nice to return a different error.
+  # If 2 is a directory, do not even try to move 1 inside it.
+  test -d "$2" && return 3
+
+  # If 2 does not exist already, and 1 exists in current directory.
+  if test ! -e "$2" -a -e "${1##*/}"; then
+    mv -v "$1" "$2"
+  else
+    # Try different directory (but never move between directories).
+    # mv will error out anyway if 1 does not exist in other location either.
+    rename_location "$1" "$2"
+  fi
 }
 
-rename_ext () {
-  test -e "${1%\.*}$2" && return 1 || mv -v "$1" "${1%\.*}$2"
+rename_extension () {
+  # If new extension version of 1 does not exist, allow rename.
+  test ! -e "${1%\.*}.$2" && mv -v "$1" "${1%\.*}.$2"
+}
+
+# E.g. rename ../Downloads/picture.jpg picture2.jpg
+# becomes rename ../Downloads/picture.jpg rename ../Downloads/picture2.jpg
+rename_location () {
+  # If 2 does not exist in same location as 1, and 2 is not another location.
+  test ! -e "${1%/*}/$2" -a "${2##*/}" == "$2" && mv -v "$1" "${1%/*}/$2"
 }
