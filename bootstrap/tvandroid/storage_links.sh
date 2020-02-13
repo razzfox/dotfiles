e1397cb5a30580f7915692006b5892d9322a0f37
Razz Fox <razzintown@gmail.com>
2020-02-12 22:31:57 -0800

    tv update

--- bootstrap/tvandroid/storage_links.sh
+++ bootstrap/tvandroid/storage_links.sh
@@ -6,25 +6,27 @@ termux-setup-storage && test -d storage && rm -r storage &
 # Note: ln -f follows links instead of replacing them without -n (no-follow)
 
 # hardcoded usbs
-# /sdcard -> /storage/self/primary -> /storage/emulated/0
+# /sdcard -> /storage/self/primary
+# /storage/self/primary -> /storage/emulated/0
 ln -sfn /storage/emulated/0 $HOME/internal
 
 # known usbs (key follows var rules: start with a letter, no punctuation)
-#[uuidsdcard]='internal'
 unset usb
-declare -A usb=( [uuid7BB31816]='external' [uuid033E1DE8]='bluestick' )
+declare -A usb=( [uuid57B326F9]='external' )
 
-for drive in /storage/[0-9]*; do
-  # basename
-  driveName=${drive##*/}
-  # check known name, no dash
-  knownUSB=${usb[uuid${driveName//-}]}
-  if test -n "${knownUSB}"; then
+for drive in /storage/*[^self][^emulated]; do
+  externalLocation=${drive}/Android/data/com.termux/files
+  driveBasename=${drive##*/}
+
+  # check usb list for uuid with no dash
+  knownUSB=${usb[uuid${driveBasename//-}]}
+
+  if test -n "${knownUSB}" -a -d "${externalLocation}"; then
     # create link using known name
-    ln -sfn ${drive}/Android/data/com.termux/files $HOME/${knownUSB}
+    ln -sfn ${externalLocation} $HOME/"${knownUSB}"
   fi
 
   mkdir -p $HOME/usb
   # create link using uuid before dash
-  ln -sfn ${drive}/Android/data/com.termux/files $HOME/usb/${driveName%%-*}
+  ln -sfn ${drive} $HOME/usb/${driveBasename%%-*}
 done
